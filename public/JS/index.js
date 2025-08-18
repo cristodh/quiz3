7149d666966a8239d4dce2749641d4ce64c3611d
cristodh <cdelgado.hidalgo1996fwd@gmail.com>
2025-08-18 15:49:24 -0600

    commit

---
+++ public/JS/index.js
@@ -0,0 +1,21 @@
+// Referenciamos todos los ids del HTML usando document.getElement
+
+
+const casilla0 = document.getElementById("casilla0");
+const casilla1 = document.getElementById("casilla1");
+const casilla2 = document.getElementById("casilla2");
+const casilla3 = document.getElementById("casilla3");
+const casilla4 = document.getElementById("casilla4");
+const casilla5 = document.getElementById("casilla5");
+const casilla6 = document.getElementById("casilla6");
+const casilla7 = document.getElementById("casilla7");
+const casilla8 = document.getElementById("casilla8");
+
+
+// Creamos un arreglo que contenga todas esas referencias (variables)
+const casillas = [casilla0, casilla1, casilla2, casilla3, casilla4, casilla5, casilla6, casilla7, casilla8];
+
+function movimientos() {
+    
+}
+movimientos()
