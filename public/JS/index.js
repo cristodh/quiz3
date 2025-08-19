2fb0d5727ee0bf56623ea1ddaef4dcd7d5645fc7
cristodh <cdelgado.hidalgo1996fwd@gmail.com>
2025-08-19 15:50:45 -0600

    08/19/2025

--- public/JS/index.js
+++ public/JS/index.js
@@ -12,10 +12,45 @@ const casilla7 = document.getElementById("casilla7");
 const casilla8 = document.getElementById("casilla8");
 
 
+
+
 // Creamos un arreglo que contenga todas esas referencias (variables)
 const casillas = [casilla0, casilla1, casilla2, casilla3, casilla4, casilla5, casilla6, casilla7, casilla8];
 
 function movimientos() {
-    
+    casillas.forEach((casilla) => {
+        casilla.addEventListener("click", function () {
+            if (casilla.textContent == "") {  //condicional para evitar que se reemplace los O
+                casilla.textContent = "X"
+                setTimeout(movCirculo, 750); //retraso en la accion de la CPU
+            }
+            
+
+        })
+
+    });
+}
+function movCirculo() {  //funcion aleatoria para marcar el O
+    const vacias = casillas.filter((box) => box.textContent == '')
+    const numAleatorio = [Math.floor(Math.random() * vacias.length)]
+    vacias[numAleatorio].textContent = 'O'
 }
 movimientos()
+
+function validarGanador() {
+    const patronesGanadores = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], //filas
+        [0, 4, 8], [2, 4, 6], //diagonales
+        [0, 3, 6], [1, 4, 7], [2, 5, 8] //columnas
+    ]
+    for (let index = 0; index < patronesGanadores.length; index++) {
+        const [pos1, pos2, pos3] = patronesGanadores[index]
+        if (patronesGanadores[pos1].textContent!="" &&
+            patronesGanadores[pos1].textContent==patronesGanadores[pos2].textContent&&
+            patronesGanadores[pos1].textContent==patronesGanadores[pos3].textContent) {
+            alert("GANO!")
+        }
+    }
+
+}
+
